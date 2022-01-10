e4ca5303e5c8829a1d7ecb4ebd744ebec3d7297a
Yusuke Yoshioka <tokyoprogramming1@outlook.com>
2022-01-10 17:28:18 +0900

    remove pics

--- photo_location2.py
+++ photo_location2.py
@@ -2,12 +2,23 @@ from GPSPhoto import gpsphoto
 import folium
 
 
-res = gpsphoto.getGPSData('image1.jpg')
+def getImageData(image):
 
-lat = res['Latitude']
-lon = res['Longitude']
+    res = gpsphoto.getGPSData(image)
+
+    dataExist = len(res)
+    if dataExist != 0:
+
+        lat = res['Latitude']
+        lon = res['Longitude']
+        print(lat, lon)
+
+        myMap = folium.Map(location=[lat, lon], zoom_start=9)
+        folium.Marker(location=[lat, lon]).add_to(myMap)
+        myMap.save('index.html')
+    else:
+        print('no data available')
 
 
-myMap = folium.Map(location=[lat, lon], zoom_start=9)
-folium.Marker(location=[lat, lon]).add_to(myMap)
-myMap.save('index.html')
+getImageData('image1.jpg')
+getImageData('image2.jpg')
