2bea75f72f1b4552225e7aada418a02fd1945b08
Yusuke Yoshioka <tokyoprogramming1@outlook.com>
2022-01-19 08:04:24 +0900

    scrape merkari && rakuma

--- scrape_mercari.py
+++ scrape_mercari.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options 
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 
@@ -19,9 +19,28 @@ driver.get('https://jp.mercari.com/item/m84680014499')
 time.sleep(10)
 
 html = driver.page_source
-soup = BeautifulSoup(html, 'lxml')
+soup = BeautifulSoup(html, 'html.parser')
 
-print(soup)
+
+name_tag = soup.find_all(attrs={'data-testid' : 'name'})
+price_tag = soup.find_all(attrs={'data-testid' : 'price'})
+heart_tag = soup.find_all(attrs={'data-testid' : 'icon-heart-button'})
+
+status_tag = soup.find_all(attrs={'data-testid': 'checkout-button'})
+
+
+
+name = name_tag[0]['title-label']
+price = price_tag[0]['value']
+heart = heart_tag[0]['label']
+status = status_tag[0].text
+
+print(name)
+print(price)
+print(heart)
+print(status)
+
+
 
 
 
